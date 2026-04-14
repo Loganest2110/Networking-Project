5f7db78b34ad7fc9c253605cc09ff49956887099
Loganest2110 <logan.donoghue@mymail.champlain.edu>
2026-04-14 13:21:12 -0400

    Added random elo generation and fixed a bug with client.

--- client.cpp
+++ client.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <string>
+#include <ctime>
 
 using boost::asio::ip::tcp;
+
+int randomElo() {
+    srand(time(0));
+    int random = (rand() % 2001) + 100;
+    return random;
+}
 
 void client() {
     boost::asio::io_context io_context;
     tcp::socket socket(io_context);
     char message[1024];
+
+    int elo = randomElo();
+    std::cout << "Your elo is: " << elo << std::endl;
 
     tcp::resolver resolver(io_context);
     auto endpoint = resolver.resolve("127.0.0.1", "5555");
@@ -17,6 +27,6 @@ void client() {
 
     boost::asio::write(socket, boost::asio::buffer("Message to server."));
 
-    size_t length = boost::asio::read(socket, boost::asio::buffer(message, 20));
+    size_t length = socket.read_some(boost::asio::buffer(message));
     std::cout << "Server reply: " << std::string(message, length) << std::endl;
 }
